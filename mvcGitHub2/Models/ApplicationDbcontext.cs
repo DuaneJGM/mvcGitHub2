4030dd71575225ddd54b2a856334bd6b6930e61b
DUANE J G M <duane_johnovan1@hotmail.com>
2021-06-27 08:54:53 -0500

    actualizacion entity

---
+++ mvcGitHub2/Models/ApplicationDbcontext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvcGitHub2.Models
+{
+    public class ApplicationDbcontext : DbContext
+
+    {
+
+
+        public DbSet<Persona> Persona { get; set; }
+    }
+
+
+
+}
